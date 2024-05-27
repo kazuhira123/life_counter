f273e534230d78f5e9be920d0decdd0b2586e98c
Izumi1997 <kohey-bsk.0720@outlook.com>
2024-05-27 22:17:16 +0900

    ObjectBoxから値を取得するための変数storeをStore型で定義 さらに、storeを作成する為のopenStoreメソッドを実行するために2つの関数を作成 （この時、 出力がFuture型のため、async awaitキーワードを使用したいが、initStateでは使えないので2つの関数を作成している）

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:life_counter/life_event.dart';
+import 'objectbox.g.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,7 +25,6 @@ class MyApp extends StatelessWidget {
 class LifeCounterPage extends StatefulWidget {
   const LifeCounterPage({super.key, required this.title});
 
-
   final String title;
 
   @override
@@ -31,9 +32,26 @@ class LifeCounterPage extends StatefulWidget {
 }
 
 class _LifeCounterPage extends State<LifeCounterPage> {
+  //ObjectBoxから値を受け取るための変数storeを定義
+  //変数の型名の前に?を入れることで、変数にnullを入れられるようにしている
+  Store? store;
+  Box<LifeEvent>? lifeEventBox;
+
+  Future<void> initialize() async {
+    store = await openStore();
+    lifeEventBox = store?.box<LifeEvent>();
+    setState(() {});
+  }
+
+  //initState()ではasync awaitが使用できないので、async awaitを使った関数を作成し、initState内で呼び出している
+  @override
+  void initState() {
+    super.initState();
+    initialize();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-    );
+    return Scaffold();
   }
 }
